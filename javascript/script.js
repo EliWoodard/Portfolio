779569aaf64790fffd81df921937f15a95edb7f0
EliWoodard <elijahwoodard136@gmail.com>
2023-08-09 23:54:37 -0600

    Working on Header Animation

--- javascript/script.js
+++ javascript/script.js
@@ -13,4 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
     if (video) {
         video.playbackRate = 2;
     }
+
+    addStars();
 });
+
+function addStars() {
+    var starsContainer = document.getElementById('stars-container');
+    for (var i = 0; i < 100; i++) {
+        var star = document.createElement('div');
+        star.className = 'star';
+        star.style.left = Math.random() * 100 + 'vw';
+        star.style.top = Math.random() * 100 + 'vh';
+        starsContainer.appendChild(star);
+    }
+}
