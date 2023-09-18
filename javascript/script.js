ad34e798f01d82f6966040743d117a308c68fcb4
EliWoodard <elijahwoodard136@gmail.com>
2023-09-17 21:38:18 -0600

    Fixed Satellite Positions

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let scene, camera, renderer, earth, clouds, satellites = [];
+    let scene, camera, renderer, earth, clouds, satellites = [], satellitesGroup;
 
     const COLORS = [0x000000, 0xffffff];  // Blank and White
     const BLINK_INTERVAL = 1000;         // Blink every second
-    const DISTANCE = 7; 
+    const DISTANCE = 6; 
 
     const header = document.querySelector("header");
     const headerWidth = header.clientWidth;
@@ -15,9 +15,9 @@ document.addEventListener("DOMContentLoaded", function() {
     
         // Create the camera
         camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.y = 4; // 3
-        camera.position.x = 0; // 0
-        camera.position.z = 8; // 7
+        camera.position.y = 3; // 3
+        camera.position.x = -8; // -8
+        camera.position.z = 7; // 7
     
         // Create the renderer
         renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -43,38 +43,37 @@ document.addEventListener("DOMContentLoaded", function() {
         scene.add(clouds);
 
         // Add satellites
-        const NUM_SATELLITES = 100;  // number of satellites in each band
-        const NUM_BANDS = 100;       // number of bands around the Earth
-        const INCLINATIONS = [Math.PI / 12, Math.PI / 6, -Math.PI / 12, -Math.PI / 6];
+        const NUM_SATELLITES = 20;  // number of satellites in each band
+        const NUM_BANDS = 20;       // number of bands around the Earth
+        const INCLINATIONS = Array.from({ length: NUM_BANDS }, (_, idx) => (Math.PI * idx / (NUM_BANDS - 1)));
         const TOTAL_SATELLITES = NUM_SATELLITES * INCLINATIONS.length;
 
+        satellitesGroup = new THREE.Group();
 
         INCLINATIONS.forEach((inclination, bandIdx) => {
             for (let i = 0; i < NUM_SATELLITES; i++) {
-                let satellite = new THREE.Mesh(
-                    new THREE.SphereGeometry(0.0025, .4, .4),
-                    new THREE.MeshBasicMaterial({ color: 0xffffff })
-                );
-        
-                // Calculate the global index for this satellite
+                let satellite = new THREE.Mesh(new THREE.SphereGeometry(0.005, .08, .08), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+
                 let globalIndex = bandIdx * NUM_SATELLITES + i;
-        
+
                 satellite.blinkTimer = (BLINK_INTERVAL / TOTAL_SATELLITES) * globalIndex;
                 satellite.colorIndex = i % 2;
-        
+
                 satellite.rotationData = {
-                    theta: (i * 2 * Math.PI / NUM_SATELLITES) + (bandIdx * Math.PI / NUM_BANDS),
+                    theta: (i * 2 * Math.PI / NUM_SATELLITES),
                     inclination: inclination
                 };
-                
+
                 satellites.push(satellite);
-                scene.add(satellite);
+                satellitesGroup.add(satellite);
             }
         });
+
+        scene.add(satellitesGroup);
     
         // Create a light source
         const light = new THREE.PointLight(0xffffff, 1);
-        light.position.set(10, 10, 10);
+        light.position.set(-12, 10, 10); // 0, 10, 10
         scene.add(light);
     
         // Handle window resize
@@ -83,43 +82,39 @@ document.addEventListener("DOMContentLoaded", function() {
         animate();
     }
 
-    function calculateWavePosition(theta, inclination, distance) {
-        const waveEffect = Math.sin(5 * theta);  // Adjust "5" to change the number of waves.
-        const adjustedInclination = inclination + 0.5 * waveEffect; // 0.5 determines the height of the wave.
-        
-        let x = distance * Math.cos(adjustedInclination) * Math.cos(theta);
-        let y = distance * Math.sin(adjustedInclination);
-        let z = distance * Math.cos(adjustedInclination) * Math.sin(theta);
-        
+    function calculateSatellitePosition(theta, inclination, distance) {
+        let x = distance * Math.sin(inclination) * Math.cos(theta);
+        let y = distance * Math.sin(inclination) * Math.sin(theta);
+        let z = distance * Math.cos(inclination);
+            
         return { x, y, z };
     }
 
-  function animate() {
-    requestAnimationFrame(animate);
-        
-    earth.rotation.y += 0.0003;
-    clouds.rotation.y += 0.0005;
-
-    satellites.forEach(satellite => {
-        satellite.rotationData.theta += 0.0001;
-        satellite.blinkTimer += 5;  
-        
-        if (satellite.blinkTimer > BLINK_INTERVAL) {
-            satellite.blinkTimer = 0;
-            satellite.colorIndex = (satellite.colorIndex + 1) % 2;
-            satellite.material.color.set(COLORS[satellite.colorIndex]);
-        }
-            
-        const position = calculateWavePosition(
-            satellite.rotationData.theta, 
-            satellite.rotationData.inclination, 
-            DISTANCE
-        );
-        satellite.position.set(position.x, position.y, position.z);
-    });
+    function animate() {
+        requestAnimationFrame(animate);
     
-    renderer.render(scene, camera);
-}
+        earth.rotation.y -= 0.0003;
+        clouds.rotation.y -= 0.0005;
+        satellitesGroup.rotation.y += 0.0001;
+    
+        satellites.forEach(satellite => {
+            satellite.rotationData.theta += 0.0001;
+            satellite.rotationData.inclination += 0.0001; // Diagonal movement
+            satellite.blinkTimer += 5;  
+            
+            if (satellite.blinkTimer > BLINK_INTERVAL) {
+                satellite.blinkTimer = 0;
+                satellite.colorIndex = (satellite.colorIndex + 1) % 2;
+                satellite.material.color.set(COLORS[satellite.colorIndex]);
+            }
+            
+            const position = calculateSatellitePosition(satellite.rotationData.theta, satellite.rotationData.inclination, DISTANCE);
+            satellite.position.set(position.x, position.y, position.z);
+        });
+    
+        renderer.render(scene, camera);
+    }
+    
 
     
     function onWindowResize() {
